2e90551c466191c188f1a77d7b684862ca7b2962
Rene <rene93h@gmail.com>
2021-02-04 17:59:07 -0600

    agregando docs

---
+++ docs/precache-manifest.02d320405fa2a84d27b74332d84c8cf3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f637b7737e8b5b2175d8c68d6b4b86d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2426eeafd76728df2a5",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2426eeafd76728df2a5",
+    "url": "/static/js/main.a11af43f.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
